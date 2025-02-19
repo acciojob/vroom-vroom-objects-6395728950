6b1ae4e9e76882c74d4a928ccfcf067ba965dc4e
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-02-20 00:23:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+	getMakeModel(){
+		string str = make+" "+model;
+		return str;
+	}
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	this.make = make;
+	this.model = model;
+	this.topSpeed = topSpeed;
+	super.car(make,model);
+	getTopSpeed(){
+		return topSpeed;
+	}
+	
+}
 
 // Do not change the code below
 window.Car = Car;
